49fbc3fcf1f7a97ccd215439d2cb5f13971c72db
jostne <espen_hansen90@hotmail.com>
2014-10-27 14:51:22 +0100

    Ny endring

--- Nettbutikkprosjekt/Controllers/KunderController.cs
+++ Nettbutikkprosjekt/Controllers/KunderController.cs
@@ -295,7 +295,6 @@ namespace Nettbutikkprosjekt.Controllers
         }
         public ActionResult Home()
         {
-            //her kommer det en endring
             return View();
         }
 
